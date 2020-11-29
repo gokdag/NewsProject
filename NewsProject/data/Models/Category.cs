488cb27437de7acbf59d0f180a303943aebba77c
Kerim Can Gökdağ <53795573+gokdag@users.noreply.github.com>
2020-11-29 23:02:51 +0300

    Initial commit

---
+++ NewsProject/data/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsProject.data.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public List<Report> Reports { get; set; }
+    }
+}
---
+++ NewsProject/data/Models/Report.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsProject.data.Models
+{
+    public class Report
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string LongTitle { get; set; }
+        public string Content { get; set; }
+        public int ReadOfNumber { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime DateTime { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
