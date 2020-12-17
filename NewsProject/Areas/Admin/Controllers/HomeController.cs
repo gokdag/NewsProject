4a5d4afeba177b3720b06a130979e59bc4dc5568
Kerim Can Gökdağ <53795573+gokdag@users.noreply.github.com>
2020-12-17 15:18:16 +0300

    AdminPage&Role

--- NewsProject/Areas/Admin/Controllers/HomeController.cs
+++ NewsProject/Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NewsProject.data.Models;
 using NewsProject.Filter;
 using System;
 using System.Collections.Generic;
@@ -9,14 +11,48 @@ using System.Threading.Tasks;
 namespace NewsProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    
     [Authorize]
     [ActionFilterSessionCheck]
     public class HomeController : Controller
     {
+        UnitOfWork unitOfWork = new UnitOfWork(new DatabaseContext());
+        RoleManager<IdentityRole> roleManager;
+
+        public HomeController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        public IActionResult Listele()
+        {
+            unitOfWork.ReportRepository.GetAllWithCategory();
+
+            return View(unitOfWork.ReportRepository);
+        }
+        public IActionResult DeleteReport(int Id)
+        {
+            unitOfWork.ReportRepository.DeleteById(Id);
+            unitOfWork.Complete();
+            return RedirectToAction("Listele", "Home", new { area = "Admin" });
+        }
+    
+        
+        /*Roles*/
         public IActionResult Index()
         {
-            return View();
+            var roles = roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            await roleManager.CreateAsync(role);
+            return RedirectToAction("Index");
         }
     }
 }
